eba4ec0ecc39b331611c1cd018a8b9ebff9a5286
Jay Prabhu <jay@classplus.co>
2021-07-03 15:54:07 +0530

    --

---
+++ src/routes/Home/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.✂ '
+
+function Header() {
+    return (
+        <div class="Top">
+            <div class="Top__Left">
+                <div class="Top__Left--item"><img src="./resources/Images/phone-white.svg" alt="" height="15px" width="15px"/>+91-8696943451</div>
+                <div class="Top__Left--item"><img src="./resources/Images/mail-white.svg" alt="" height="15px" width="15px"/>patterns@gmail.com</div>
+            </div>
+            <div class="Top__Right">
+                <img src="./resources/Images/fb-white.svg" alt="" height="18px" width="18px"/>
+                <img src="./resources/Images/tw-white.svg" alt="" height="18px" width="18px"/>
+                <img src="./resources/Images/ig-white.svg" alt="" height="18px" width="18px"/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
